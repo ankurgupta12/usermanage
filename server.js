8f87d41590df59728c485f9d8689c53b2c8ec3a3
ankurgupta12 <38654295+ankurgupta12@users.noreply.github.com>
2018-07-10 17:26:15 +0530

    add file

---
+++ server.js
@@ -0,0 +1,32 @@
+require('rootpath')();
+var express = require('express');
+var app = express();
+var cors = require('cors');
+var md5 = require('md5');
+var bodyParser = require('body-parser');
+var config = require('./config/config.json');
+
+var expressJWT = require('express-jwt');
+process.env.NODE_ENV = 'development';
+var env = process.env.NODE_ENV;
+if (env === 'test' || env === 'development') {
+    var envConfig = config[env];
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key];
+    })
+}
+var port = process.env.port || 5000;
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// jwt token
+
+app.use('/userReg', require('./controller/userRegistration'));
+
+app.listen(port, () => {
+    console.log('started on port' + port);
+});
+
+
+
