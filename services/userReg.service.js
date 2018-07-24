1a880e1661d67700bae40bd44b5eed1622c50e85
Unknown <ankrgpta.bhpr@gmail.com>
2018-07-24 06:24:09 +0530

    add new node modules

--- services/userReg.service.js
+++ services/userReg.service.js
@@ -2,15 +2,17 @@ var config = require('./../config/config.json');
 var Q = require('q');
 var _ = require('lodash');
 var mongo = require('mongoskin');
-var db = mongo.db(config.development.MONGO_DB_URI,{native_parser:true});
+console.log(config.development.MONGODB_URI);
+var db = mongo.db(config.development.MONGODB_URI,{ useNewUrlParser: true });
 db.bind('registraion');
+var bcrypt = require('bcryptjs');
 var service = {};
 service.registration = registration;
 module.exports = service;
 
 function registration(ReqParam){
 var deferred = Q.defer();
-db.registraion.findOne({username:ReqParam.userName,mobileno:ReqParam.mobileno},function(err,user){
+db.registraion.findOne({username:ReqParam.username,mobileno:ReqParam.mobileno},function(err,user){
     if(err) deferred.reject(err.name+':'+err.message);
     if(user){
         deferred.reject('already exist');
@@ -19,13 +21,16 @@ db.registraion.findOne({username:ReqParam.userName,mobileno:ReqParam.mobileno},f
         createUser(ReqParam);
     }
 });
+return deferred.promise;
 }
 function createUser(ReqParam) {
+    var deferred = Q.defer();
     var user = _.omit(ReqParam,'password');
     user.hash = bcrypt.hashSync(ReqParam.password,10);
     db.registraion.insert(user,function(err,doc){
+        console.log(doc);
         if(err) deferred.reject();
-        deferred.resolve();
+        deferred.resolve(doc);
     });
-    return deferred.promise();
+    return deferred.promise;
 }
